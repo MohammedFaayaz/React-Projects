5a3950e9bacf1516d822d73f80bbbb97f04cf540
MohammedFaayaz <fayaz96607@gmail.com>
2025-04-14 21:26:16 +0530

    changes

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Fayaz from './Fayaz';
+
+function App() {
+  return (
+    <>
+   <h1>MY REACT APP </h1>
+   <Fayaz/>
+   </>
+  );
+}
+
+export default App;
---
+++ 01basicreact/src/Fayaz.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+function Fayaz(){
+    return (
+        <h1>Fayaz React App</h1>
+    );
+}
+
+export default Fayaz;
